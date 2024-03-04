8bc5ce62671c67358b0f1ef499986873a0e93b6b
hetao <hetao>
2024-03-04 14:08:48 +0800

    20240304-数据结构与算法队列

--- study-optimize/src/main/java/com/atguigu/algorithm/queue/ArrayQueueDemo.java
+++ study-optimize/src/main/java/com/atguigu/algorithm/queue/ArrayQueueDemo.java
@@ -6,7 +6,42 @@ public class ArrayQueueDemo {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        System.out.println("请输入队列的方法");
+        ArrayQueue arrayQueue = new ArrayQueue(3);
+        boolean loop = true;
+        while (loop) {
+            System.out.println("isEmpty：判断当前队列是否为空");
+            System.out.println("isFull：判断当前队列是否已满");
+            System.out.println("add：向队列中添加元素");
+            System.out.println("remove：移除队列的元素");
+            System.out.println("show:显示头部元");
+            String value = scanner.nextLine();
+            switch (value) {
+                case "isEmpty":
+                    System.out.println("当前数组队列是否为空?" + arrayQueue.isEmpty());
+                    break;
+                case "isFull":
+                    System.out.println("当前数组队列是否已满?" + arrayQueue.isFull());
+                    break;
+                case "add":
+                    System.out.println("请输入需要添加的元素");
+                    int i = scanner.nextInt();
+                    arrayQueue.addQueue(i);
+                    break;
+                case "remove":
+                    arrayQueue.removeQueue();
+                    break;
+                case "show":
+                    arrayQueue.showHeadQueue();
+                    break;
+                case "exit":
+                    System.out.println("退出当前队列操作!");
+                    loop = false;
+                    break;
+                default: break;
+            }
+            System.out.println("队列元素个数为:" + arrayQueue.number());
+        }
     }
 }
 
@@ -25,7 +60,7 @@ class ArrayQueue{
      * 判断数组队列是否为空
      * @return
      */
-    private boolean isEmpty(){
+    public boolean isEmpty(){
         return front == rear;
     }
 
@@ -33,7 +68,7 @@ class ArrayQueue{
      * 判断数值中队列是否已满
      * @return
      */
-    private boolean isFull(){
+    public boolean isFull(){
         return front + 1 == maxSize;
     }
 
@@ -41,7 +76,7 @@ class ArrayQueue{
      * 添加队列
      * @param number
      */
-    private void addQueue(int number){
+    public void addQueue(int number){
         if (isFull()) {
             System.out.println("数组队列已满，无法添加...");
             return;
@@ -53,7 +88,7 @@ class ArrayQueue{
     /**
      * 移除队列
      */
-    private void removeQueue(){
+    public void removeQueue(){
         if (isEmpty()) {
             System.out.println("数组队列为空，无法移除元素");
             return;
@@ -62,11 +97,15 @@ class ArrayQueue{
         arr[rear] = 0;
     }
 
-    private int showHeadQueue(){
+    public int showHeadQueue(){
         if (isEmpty()) {
             System.out.println("队列为空，无法显示头部数值!");
             return 0;
         }
         return arr[front];
+    }
+
+    public int number(){
+        return front - rear;
     }
 }
